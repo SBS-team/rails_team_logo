eb9480758ffc111c850021eff9f09a255941ef86
dark-def <drkmen@rambler.ru>
2013-08-17 15:42:52 +0300

    notice

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -2,17 +2,46 @@ require "rails_team_logo/version"
 
 module RailsTeamLogo
 
-  class Server < ::Rack::Server
+  #class Server < ::Rack::Server
+  #
+  #  def start
+  #    puts '='*100
+  #    puts '='*100
+  #    puts '='*100
+  #    puts '='*100
+  #
+  #    super
+  #
+  #    puts '='*100
+  #    puts '='*100
+  #    puts '='*100
+  #    puts '='*100
+  #  end
+  #
+  #end
 
-    def start
-      super
+  #config/application.rb
+  #  config.after_initialize do
+  #    puts '='*200
+  #    ActionView::Base.sanitized_allowed_tags.delete 'div'
+  #  end
+  # Нужно как-то в config/application.rb дописывать это и вызывать там описаный ЗДЕСЬ метод. THINK
 
-      puts '='*100
-      puts '='*100
-      puts '='*100
-      puts '='*100
-    end
-
-  end
+  #def model_contents
+  #  buffer = <<-CONTENT
+  ## Include default devise modules. Others available are:
+  ## :token_authenticatable, :confirmable,
+  ## :lockable, :timeoutable and :omniauthable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
+  #
+  #  CONTENT
+  #  buffer += <<-CONTENT if needs_attr_accessible?
+  ## Setup accessible (or protected) attributes for your model
+  #attr_accessible :email, :password, :password_confirmation, :remember_me
+  #
+  #  CONTENT
+  #  buffer
+  #end
 
 end
