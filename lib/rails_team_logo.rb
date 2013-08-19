655d4f75a30e482e7855ebf6372351af3215e012
dark-def <drkmen@rambler.ru>
2013-08-19 16:32:39 +0300

    fix

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -12,7 +12,7 @@ module RailsTeamLogo
       puts '_'*100
       params.each do |key,value|
         puts '#'*100
-        puts key[value]
+        puts key["#{value}"]
       end
       #print params['params']['team_name']
     end
