5cf9600617144512fe4dd6efb0e31764f5be777f
dark-def <drkmen@rambler.ru>
2013-08-19 16:05:21 +0300

    improves

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,10 +3,17 @@ require "yaml"
 
 module RailsTeamLogo
 
+  def get_params
+    return YAML.load_file("logo.yml")
+  end
+
   class MyRailtie < Rails::Railtie
-    params = YAML.load_file("logo.yml")
+    params = get_params
     config.after_initialize do
-      print params['params']['team_name']
+      params.each do |f|
+        puts f
+      end
+      #print params['params']['team_name']
     end
   end
 
