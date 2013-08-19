b6a3b95cee4ffc7b19be8838cde0eaa0e0705ac2
dark-def <drkmen@rambler.ru>
2013-08-19 13:47:25 +0300

    stable output from file

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -6,8 +6,9 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
     params = YAML.load_file("logo.yml")
     config.after_initialize do
-      puts '='*200
+      puts '='*100
       puts params['params']['team_name']
+      puts '='*100
       ############
       ############
       ############
