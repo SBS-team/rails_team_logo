20e595f4d5808bc5efe97916bd92139b6c3a9ab9
dark-def <drkmen@rambler.ru>
2013-08-19 13:43:57 +0300

    and one more

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -4,7 +4,7 @@ require "yaml"
 module RailsTeamLogo
 
   class MyRailtie < Rails::Railtie
-    params = YAML.load_file("#{RAILS.root}logo.yml")
+    params = YAML.load_file("logo.yml")
     config.after_initialize do
       puts '='*200
       puts params['params']['team_name']
