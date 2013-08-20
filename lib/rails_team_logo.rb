0f086637b935f5d396b20434cea6115e8281838e
dark-def <drkmen@rambler.ru>
2013-08-20 19:33:39 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,14 +3,17 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
 
     begin
-      puts require './config/initializers/logo.rb'
-      config.after_initialize do
-        Params.each do |key,value|
-          puts '#'*100
-          puts value
-          puts '#'*100
+      if File.exists?("#{Rails.root}/config/initializers/logo.rb")
+        require './config/initializers/logo.rb'
+        config.after_initialize do
+          Params.each do |key,value|
+            puts '#'*100
+            puts value
+            puts '#'*100
+          end
         end
       end
+      puts 'net file'
     rescue
       puts '123'
       #copy_file "../../../lib/generators/rails_team_logo/templates/logo.rb", "config/initializers/logo.rb"
