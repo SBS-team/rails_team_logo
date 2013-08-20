16f481dd4242e6488ee0477b8f58261af69d60fe
dark-def <drkmen@rambler.ru>
2013-08-20 19:24:25 +0300

    try

--- lib/generators/rails_team_logo/install_generator.rb
+++ lib/generators/rails_team_logo/install_generator.rb
@@ -5,7 +5,6 @@ module RailsTeamLogo
       source_root File.expand_path("../../templates", __FILE__)
 
       def copy_locale
-        puts "123123123123123123123123"
         copy_file "../../../lib/generators/rails_team_logo/templates/logo.rb", "config/initializers/logo.rb"
       end
 
--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,10 +3,7 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
 
     begin
-      config.before_initiliaze do
-        copy_file "../../../lib/generators/rails_team_logo/templates/logo.rb", "config/initializers/logo.rb"
-      end
-      require './config/initializers/logo.rb'
+      puts require './config/initializers/logo.rb'
       config.after_initialize do
         Params.each do |key,value|
           puts '#'*100
@@ -15,6 +12,7 @@ module RailsTeamLogo
         end
       end
     rescue
+      puts '123'
       #copy_file "../../../lib/generators/rails_team_logo/templates/logo.rb", "config/initializers/logo.rb"
     end
 
