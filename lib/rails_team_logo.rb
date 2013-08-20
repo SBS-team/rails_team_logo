422a6a6130fec70232e776b37cac3625f9218c7b
dark-def <drkmen@rambler.ru>
2013-08-20 18:30:28 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,7 +3,7 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
 
     begin
-      include './config/initializers/logo.rb'
+      require './config/initializers/logo.rb'
       config.after_initialize do
 
         Params.each do |key,value|
