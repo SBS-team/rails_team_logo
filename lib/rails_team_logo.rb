b7418e49136eb31af90702943cfaeca3a9c99ed3
dark-def <drkmen@rambler.ru>
2013-08-20 17:54:09 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -1,55 +1,11 @@
-#require "rails_team_logo/version"
-#
-#module RailsTeamLogo
-#
-#  Params = Hash.new()
-#
-#  class MyRailtie < Rails::Railtie
-#
-#    begin
-#      #params = YAML.load_file("config/logo.rb")
-#
-#      config.after_initialize do
-#        p Params.inspect
-#        Params.each do |key,value|
-#          puts '#'*100
-#          puts value
-#          puts '#'*100
-#        end
-#      end
-#    rescue
-#
-#    end
-#
-#  end
-#
-#
-#
-#end
-
-#
-#require 'logo.rb'
-#"/home/user9/work/gemtest"
-#require 'config/initializers/logo'
 module RailsTeamLogo
 
-
-
   class MyRailtie < Rails::Railtie
-    #@params = Hash.new()
-    begin
-      #require 'config/initializers/logo.rb'
-      #Params = load_file("config/initializers/logo.rb")
-      require './config/initializers/logo.rb'
-      #load_file("config/initializers/logo.rb")
-      p Dir.pwd
-      #include 'config/initializers/logo.rb'
 
+    begin
+      require './config/initializers/logo.rb'
       config.after_initialize do
 
-        #require 'config/initializers/logo.rb'
-
-        p Params.inspect
         Params.each do |key,value|
           puts '#'*100
           puts value
@@ -57,11 +13,9 @@ module RailsTeamLogo
         end
       end
     rescue
-      p 'rescue come on'
+
     end
 
   end
-
-
 
 end
