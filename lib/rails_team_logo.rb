9a4029867ca1e694cbecc9d041c332a7b8238438
dark-def <drkmen@rambler.ru>
2013-08-20 19:51:28 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,8 +3,10 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
 
     begin
-      load '../../../lib/generators/rails_team_logo/templates/logo.rb'
+
+
       config.after_initialize do
+        require './config/initializers/logo.rb'
         Params.each do |key,value|
           puts '#'*100
           puts value
