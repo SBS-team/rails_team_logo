8ed1099d1e34719fcb24d4908efc3847c2743f1a
dark-def <drkmen@rambler.ru>
2013-08-20 19:01:56 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -3,7 +3,9 @@ module RailsTeamLogo
   class MyRailtie < Rails::Railtie
 
     begin
-      #require './config/initializers/logo.rb'
+      unless require './config/initializers/logo.rb'
+        copy_file "../../../lib/generators/rails_team_logo/templates/logo.rb", "config/initializers/logo.rb"
+      end
       Params = {:a => '1', :b => '2'}
       config.after_initialize do
         Params.each do |key,value|
