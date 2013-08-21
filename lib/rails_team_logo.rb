63bde7c127c5560028c4281a67790db9fc029256
dark-def <drkmen@rambler.ru>
2013-08-21 13:21:39 +0300

    try

--- lib/rails_team_logo.rb
+++ lib/rails_team_logo.rb
@@ -6,12 +6,18 @@ module RailsTeamLogo
         config.after_initialize do
           if File.exists?("#{Rails.root}/config/initializers/logo.rb")
             require './config/initializers/logo.rb'
-            Params.each do |key,value|
-              puts '#'*100
-              puts value
-              puts '#'*100
-            end
+          else
+            require '../../../lib/generators/rails_team_logo/templates/logo.rb'
           end
+
+          Params.each do |key,value|
+            puts '#'*100
+            puts value
+            puts '#'*100
+          end
+
+
+
         end
     rescue
 
